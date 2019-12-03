f7cc4dc9b4d690fce28585ef81b6bc4d2b4f990b
sherotree <wytreeshero@163.com>
2019-12-03 15:18:20 +0800

    bug

--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './PhotoGallery.css'
 
 export function PhotoGallery() {
   const images = [
@@ -6,74 +7,67 @@ export function PhotoGallery() {
       id: 1,
       name: '雏田',
       image:
-        'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%A3%81%E7%BA%B8%20%E5%8D%A1%E9%80%9A%E5%8A%A8%E6%BC%AB%20%E7%81%AB%E5%BD%B1%E5%BF%8D%E8%80%85&step_word=&hs=0&pn=93&spn=0&di=3630&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=&cl=&lm=-1&st=-1&cs=2343882896%2C3900438165&os=3999767816%2C836955442&simid=0%2C0&adpicid=0&lpn=0&ln=3570&fr=&fmq=1567133149621_R&fm=&ic=0&s=undefined&hd=0&latest=0&copyright=0&se=&sme=&tab=0&width=1280&height=960&face=undefined&ist=&jit=&cg=wallpaper&bdtype=0&oriquery=&objurl=http%3A%2F%2Fimage.shuoman.com%2Ffile%2Fuserfiles%2Fimages%2Fauto81%2F2017041110291712803.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3F4_z%26e3Bhwg4wg_z%26e3Bv54AzdH3FgjofAzdH3FktzitAzdH3Fn9cncd_z%26e3Bip4s&gsm=&rpstart=0&rpnum=0&islist=&querylist=&force=undefined',
+        'http://img1.imgtn.bdimg.com/it/u=2343882896,3900438165&fm=214&gp=0.jpg',
     },
     {
       id: 2,
       name: '井野',
-      image:
-        'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%A3%81%E7%BA%B8%20%E5%8D%A1%E9%80%9A%E5%8A%A8%E6%BC%AB%20%E7%81%AB%E5%BD%B1%E5%BF%8D%E8%80%85&step_word=&hs=0&pn=37&spn=0&di=84590&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=&cl=&lm=-1&st=-1&cs=532481452%2C87732447&os=1411245623%2C1881983440&simid=0%2C0&adpicid=0&lpn=0&ln=3570&fr=&fmq=1567133149621_R&fm=&ic=0&s=undefined&hd=0&latest=0&copyright=0&se=&sme=&tab=0&width=&height=&face=undefined&ist=&jit=&cg=wallpaper&bdtype=0&oriquery=&objurl=http%3A%2F%2Fdik.img.kttpdq.com%2Fpic%2F3%2F1551%2F56acacffa06a4ff4.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3B157thwg_z%26e3Bv54AzdH3Fhp14AzdH3Fiy6zAzdH3F8cc8_z%26e3Bip4s&gsm=&rpstart=0&rpnum=0&islist=&querylist=&force=undefined',
+      image: 'http://dik.img.kttpdq.com/pic/3/1551/56acacffa06a4ff4.jpg',
     },
     {
       id: 3,
       name: '鸣人',
-      image:
-        'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%A3%81%E7%BA%B8%20%E5%8D%A1%E9%80%9A%E5%8A%A8%E6%BC%AB%20%E7%81%AB%E5%BD%B1%E5%BF%8D%E8%80%85&step_word=&hs=0&pn=63&spn=0&di=3640&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=&cl=&lm=-1&st=-1&cs=455063257%2C3333878707&os=2886979689%2C1602713118&simid=0%2C0&adpicid=0&lpn=0&ln=3570&fr=&fmq=1567133149621_R&fm=&ic=0&s=undefined&hd=0&latest=0&copyright=0&se=&sme=&tab=0&width=&height=&face=undefined&ist=&jit=&cg=wallpaper&bdtype=15&oriquery=&objurl=http%3A%2F%2Fuploads.5068.com%2Fallimg%2F1802%2F179-1P201191214.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3Bcamb_z%26e3Bv54AzdH3Fp7rtwgAzdH3F14prAzdH3F8macdn_l_z%26e3Bip4s&gsm=&rpstart=0&rpnum=0&islist=&querylist=&force=undefined',
+      image: 'http://uploads.5068.com/allimg/1802/179-1P201191214.jpg',
     },
     {
       id: 4,
       name: '我爱罗',
       image:
-        'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%A3%81%E7%BA%B8%20%E5%8D%A1%E9%80%9A%E5%8A%A8%E6%BC%AB%20%E7%81%AB%E5%BD%B1%E5%BF%8D%E8%80%85&step_word=&hs=0&pn=116&spn=0&di=92400&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=&cl=&lm=-1&st=-1&cs=4055843527%2C1721626165&os=1355696847%2C2798481569&simid=0%2C0&adpicid=0&lpn=0&ln=3570&fr=&fmq=1567133149621_R&fm=&ic=0&s=undefined&hd=0&latest=0&copyright=0&se=&sme=&tab=0&width=&height=&face=undefined&ist=&jit=&cg=wallpaper&bdtype=0&oriquery=&objurl=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201501%2F26%2F20150126151152_jBTe4.thumb.224_0.jpeg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3B17tpwg2_z%26e3Bv54AzdH3Fks52fAzdH3Fpw2AzdH3F%3Fgw4j%3D%EC%BD%BD&gsm=&rpstart=0&rpnum=0&islist=&querylist=&force=undefined',
+        'http://b-ssl.duitang.com/uploads/item/201501/26/20150126151152_jBTe4.thumb.224_0.jpeg',
     },
     {
       id: 5,
       name: '小李',
       image:
-        'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%A3%81%E7%BA%B8%20%E5%8D%A1%E9%80%9A%E5%8A%A8%E6%BC%AB%20%E7%81%AB%E5%BD%B1%E5%BF%8D%E8%80%85&step_word=&hs=0&pn=376&spn=0&di=66660&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=&cl=&lm=-1&st=-1&cs=775080899%2C3239159072&os=179037170%2C1472804248&simid=3051235039%2C3871555840&adpicid=0&lpn=0&ln=3570&fr=&fmq=1567133149621_R&fm=&ic=0&s=undefined&hd=0&latest=0&copyright=0&se=&sme=&tab=0&width=&height=&face=undefined&ist=&jit=&cg=wallpaper&bdtype=0&oriquery=&objurl=http%3A%2F%2Ft-1.tuzhan.com%2Ffef828fad2c5%2Fc-2%2Fl%2F2013%2F02%2F01%2F00%2F2050822763e343bd913b46196164a121.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3Bp7ziwg_z%26e3Bv54AzdH3FowkzAzdH3Fd1un8aac9dj1lnwn_z%26e3Bip4s&gsm=&rpstart=0&rpnum=0&islist=&querylist=&force=undefined',
+        'http://img1.imgtn.bdimg.com/it/u=775080899,3239159072&fm=26&gp=0.jpg',
     },
     {
       id: 6,
       name: '自来也',
       image:
-        'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%A3%81%E7%BA%B8%20%E5%8D%A1%E9%80%9A%E5%8A%A8%E6%BC%AB%20%E7%81%AB%E5%BD%B1%E5%BF%8D%E8%80%85&step_word=&hs=0&pn=386&spn=0&di=124860&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=&cl=&lm=-1&st=-1&cs=1027929469%2C246882874&os=1375021596%2C3676324963&simid=3437122094%2C120312520&adpicid=0&lpn=0&ln=3570&fr=&fmq=1567133149621_R&fm=&ic=0&s=undefined&hd=0&latest=0&copyright=0&se=&sme=&tab=0&width=&height=&face=undefined&ist=&jit=&cg=wallpaper&bdtype=15&oriquery=&objurl=http%3A%2F%2Fdik.img.kttpdq.com%2Fpic%2F134%2F93294%2F2b6f63eac0d45068_200x150.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3B157thwg_z%26e3Bv54AzdH3Fhp14AzdH3FqyvAzdH3Fmda0_89_z%26e3Bip4s&gsm=&rpstart=0&rpnum=0&islist=&querylist=&force=undefined',
+        'http://dik.img.kttpdq.com/pic/134/93294/2b6f63eac0d45068_200x150.jpg',
     },
     {
       id: 7,
       name: '卡卡西',
       image:
-        'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%A3%81%E7%BA%B8%20%E5%8D%A1%E9%80%9A%E5%8A%A8%E6%BC%AB%20%E7%81%AB%E5%BD%B1%E5%BF%8D%E8%80%85&step_word=&hs=0&pn=405&spn=0&di=47420&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=&cl=&lm=-1&st=-1&cs=3921403971%2C2455720199&os=398410144%2C3448345141&simid=3482857169%2C466904742&adpicid=0&lpn=0&ln=3570&fr=&fmq=1567133149621_R&fm=&ic=0&s=undefined&hd=0&latest=0&copyright=0&se=&sme=&tab=0&width=&height=&face=undefined&ist=&jit=&cg=wallpaper&bdtype=15&oriquery=&objurl=http%3A%2F%2Fwww.laomaotaopan.com%2Fzhuti%2FUploadPic%2F2016-11%2Fsoft_20161113151325785.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3Bsw54w5pw5rwg_z%26e3Bv54AzdH3Fzi7ptAzdH3Fip4sAzdH3F9ccn_z%26e3Bip4s&gsm=&rpstart=0&rpnum=0&islist=&querylist=&force=undefined',
+        'http://www.laomaotaopan.com/zhuti/UploadPic/2016-11/soft_20161113151325785.jpg',
     },
     {
       id: 8,
       name: '鹿丸',
       image:
-        'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%A3%81%E7%BA%B8%20%E5%8D%A1%E9%80%9A%E5%8A%A8%E6%BC%AB%20%E7%81%AB%E5%BD%B1%E5%BF%8D%E8%80%85&step_word=&hs=0&pn=521&spn=0&di=4070&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=&cl=&lm=-1&st=-1&cs=1969370364%2C1160313707&os=1551403786%2C310262320&simid=3398608777%2C244633280&adpicid=0&lpn=0&ln=3570&fr=&fmq=1567133149621_R&fm=&ic=0&s=undefined&hd=0&latest=0&copyright=0&se=&sme=&tab=0&width=&height=&face=undefined&ist=&jit=&cg=wallpaper&bdtype=0&oriquery=&objurl=http%3A%2F%2Fpic2.cxtuku.com%2F00%2F08%2F46%2Fb271dd2ef75c.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3Bvxp7h7_z%26e3Bv54AzdH3Frtv_b9md08_z%26e3Bip4s&gsm=&rpstart=0&rpnum=0&islist=&querylist=&force=undefined',
+        'http://img5.imgtn.bdimg.com/it/u=1969370364,1160313707&fm=26&gp=0.jpg',
     },
     {
       id: 9,
       name: '牙',
-      image:
-        'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%A3%81%E7%BA%B8%20%E5%8D%A1%E9%80%9A%E5%8A%A8%E6%BC%AB%20%E7%81%AB%E5%BD%B1%E5%BF%8D%E8%80%85&step_word=&hs=0&pn=594&spn=0&di=71950&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=&cl=&lm=-1&st=-1&cs=1777041270%2C1659035128&os=1588605000%2C925272320&simid=4230931734%2C782370209&adpicid=0&lpn=0&ln=3570&fr=&fmq=1567133149621_R&fm=&ic=0&s=undefined&hd=0&latest=0&copyright=0&se=&sme=&tab=0&width=&height=&face=undefined&ist=&jit=&cg=wallpaper&bdtype=15&oriquery=&objurl=http%3A%2F%2Fimg.tukexw.com%2Fimg%2F5a6b15a029ee91bc.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Frtv_z%26e3Bp7hjxo_z%26e3Bv54AzdH3FyAzdH3Fu1r2n1d1r2e18jg23u0jg2su01q271z1AzdH3F&gsm=&rpstart=0&rpnum=0&islist=&querylist=&force=undefined',
+      image: 'http://img.tukexw.com/img/5a6b15a029ee91bc.jpg',
     },
   ]
-  function Show(props) {
-    return (
-      <div>
-        {images.map(item => (
-          <img
-            className="images"
-            src={item.image}
-            title={item.name}
-            key={item.id}
-          />
-        ))}
-      </div>
-    )
-  }
+
+  function handleAmplify(e) {}
   return (
     <div className="wrapper">
-      <Show />
+      {images.map(item => (
+        <img
+          className="images"
+          src={item.image}
+          title={item.name}
+          key={item.id}
+          onClick={handleAmplify}
+        />
+      ))}
     </div>
   )
 }
